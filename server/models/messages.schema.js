687f78797ed07418718a911672125025649ffbd6
Antonio Rice <ricex407@gmail.com>
2017-07-24 13:37:11 -0500

    basic file structure set

---
+++ server/models/messages.schema.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create the Schema
+var messagesSchema = new Schema({
+  name: {type: String},
+  message: {type: String}
+});
+
+// export our model
+module.exports = mongoose.model('Messages', messagesSchema, 'messages');
---
+++ server/server.js
@@ -0,0 +1,44 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var mongoose = require('mongoose');
+//routes
+var listings = require('./routes/messages.js');
+var index = require('./routes/index.js');
+
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.json()); // needed for angular requests
+
+/** ---------- EXPRESS ROUTES ---------- **/
+app.use('/messages', messages);
+app.use('/', index);
+
+
+/** -------- MONGOOSE CONNECTION --------**/
+var databaseUrl = 'mongodb://localhost:27017/messages';
+mongoose.connect(databaseUrl , function(err){
+  if(err){
+    console.log('error connecting to the db', err);
+  }else{
+    console.log("Connected just fine!");
+  }
+});
+
+mongoose.connection.on('connected', function() {
+  console.log('mongoose connected to : ', databaseUrl);
+});
+
+mongoose.connection.on('error', function(err) {
+  console.log('mongoose connection error: ', err);
+});
+
+/** ---------- START SERVER ---------- **/
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+    console.log('Listening on port: ', app.get('port'));
+});
